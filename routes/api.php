3480c00a6d6188e1ab4a26cc664249a1f35b4676
AMK1215 <aungmyokhaing1215@gmail.com>
2024-12-02 12:16:47 +0630

    login check

--- routes/api.php
+++ routes/api.php
@@ -60,6 +60,7 @@ Route::post('Reward', [RewardController::class, 'handleReward']);
 
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('GameLogin', [LaunchGameController::class, 'LaunchGame']);
     Route::get('wager-logs', [WagerController::class, 'index']);
     Route::get('transactions', [TransactionController::class, 'index']);
 
