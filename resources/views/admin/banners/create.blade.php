07154476d773936389e5ca6de9aeeea19da957d1
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-12-06 23:16:15 +0700

    a

--- resources/views/admin/banners/create.blade.php
+++ resources/views/admin/banners/create.blade.php
@@ -141,9 +141,17 @@
 @endsection
 @section('scripts')
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
-
 <script src="{{ asset('admin_app/assets/js/plugins/choices.min.js') }}"></script>
 <script src="{{ asset('admin_app/assets/js/plugins/quill.min.js') }}"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.32/dist/sweetalert2.all.min.js"></script>
-
+<script>
+  $(document).ready(function() {
+    $("#single").checked(function(){
+      alert("Single");
+    });
+    $("#all").checked(function(){
+      alert("All");
+    });
+  });
+</script>
 @endsection
