0e88d9f02c6e36a19d4548f5434c436388caa82e
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-12-15 00:02:13 +0700

    a

--- app/Http/Controllers/Api/V1/Player/WithDrawController.php
+++ app/Http/Controllers/Api/V1/Player/WithDrawController.php
@@ -6,9 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\WithdrawRequest;
 use App\Http\Resources\Api\V1\WithdrawResource;
 use App\Models\WithDrawRequest as ModelsWithDrawRequest;
+use App\Services\ApiService;
 use App\Traits\HttpResponses;
 use Exception;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class WithDrawController extends Controller
 {
@@ -16,13 +19,10 @@ class WithDrawController extends Controller
 
     public function withdraw(WithdrawRequest $request)
     {
-        $inputs = $request->validated();
-        $player = Auth::user();
+        try {
+            $inputs = $request->validated();
+            $player = Auth::user();
 
-<<<<<<< HEAD
-        if ($player->balanceFloat < $inputs['amount']) {
-            return $this->error('', 'Insufficient balance', 401);
-=======
             if ($player->balanceFloat < $inputs['amount']) {
                 return $this->error('', ['amount' => 'Insufficient balance'], 422);
             }
@@ -38,17 +38,6 @@ class WithDrawController extends Controller
             return $this->success(new WithdrawResource($withdraw), 'Withdraw Request Success');
         } catch (Exception $e) {
             return $this->error('', $e->getMessage(), 422);
->>>>>>> f6a1cd8 (a)
         }
-
-        $withdraw = ModelsWithDrawRequest::create(array_merge(
-            $inputs,
-            [
-                'user_id' => $player->id,
-                'agent_id' => $player->agent_id,
-            ]
-        ));
-
-        return $this->success(new WithdrawResource($withdraw), 'Withdraw Request Success');
     }
 }
