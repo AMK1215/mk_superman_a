e30e3ea50219b8189b20b345b5272dafc5840c1c
AMK1215 <aungmyokhaing1215@gmail.com>
2024-12-31 10:17:18 +0630

    GetGameProvider

--- app/Http/Controllers/Api/V1/Slot/GetGameProviderController.php
+++ app/Http/Controllers/Api/V1/Slot/GetGameProviderController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api\V1\Slot;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Services\GetGameProviderService;
+use Illuminate\Support\Facades\Log;
+
 
 class GetGameProviderController extends Controller
 {
@@ -15,8 +17,9 @@ class GetGameProviderController extends Controller
         $this->getGameProviderService = $getGameProviderService;
     }
 
-    public function fetchGameProviders()
+    public function fetchGameProviders(Request $request)
     {
+         Log::info('Incoming Request to GetGameProvider', $request->all());
         $response = $this->getGameProviderService->getGameProvider();
 
         if (isset($response['ErrorCode']) && $response['ErrorCode'] == 0) {
