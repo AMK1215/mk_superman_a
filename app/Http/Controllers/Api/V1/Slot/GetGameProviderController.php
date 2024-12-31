c37634cfff8e65f34e2b4a43446f8dd4279344a2
AMK1215 <aungmyokhaing1215@gmail.com>
2024-12-31 10:23:48 +0630

    GetGameProvider

--- app/Http/Controllers/Api/V1/Slot/GetGameProviderController.php
+++ app/Http/Controllers/Api/V1/Slot/GetGameProviderController.php
@@ -18,20 +18,40 @@ class GetGameProviderController extends Controller
     }
 
     public function fetchGameProviders(Request $request)
-    {
-         Log::info('Incoming Request to GetGameProvider', $request->all());
-        $response = $this->getGameProviderService->getGameProvider();
+{
+    Log::info('Incoming Request to GetGameProvider', $request->all());
 
-        if (isset($response['ErrorCode']) && $response['ErrorCode'] == 0) {
-            return response()->json([
-                'success' => true,
-                'data' => $response['Data'],
-            ]);
-        }
+    $response = $this->getGameProviderService->getGameProvider();
 
+    if (isset($response['status']) && $response['status'] == 200) {
         return response()->json([
-            'success' => false,
-            'message' => $response['ErrorMessage'] ?? 'An error occurred',
-        ], 400);
+            'success' => true,
+            'data' => $response['GameProviders'] ?? [],
+        ]);
     }
+
+    return response()->json([
+        'success' => false,
+        'message' => $response['Description'] ?? 'An error occurred',
+    ], 400);
+}
+
+
+    // public function fetchGameProviders(Request $request)
+    // {
+    //      Log::info('Incoming Request to GetGameProvider', $request->all());
+    //     $response = $this->getGameProviderService->getGameProvider();
+
+    //     if (isset($response['ErrorCode']) && $response['ErrorCode'] == 0) {
+    //         return response()->json([
+    //             'success' => true,
+    //             'data' => $response['Data'],
+    //         ]);
+    //     }
+
+    //     return response()->json([
+    //         'success' => false,
+    //         'message' => $response['ErrorMessage'] ?? 'An error occurred',
+    //     ], 400);
+    // }
 }
