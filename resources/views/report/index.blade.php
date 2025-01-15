145ae834aacb3d771fcfffcc589d47d49b480128
sophia <thangneimawi@hivelocity.co.jp>
2025-01-15 21:05:51 +0700

    fixed date

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -20,14 +20,14 @@
                         <div class="col-md-3">
                             <div class="input-group input-group-static mb-4">
                                 <label for="">StartDate</label>
-                                <input type="datetime-local" class="form-control" name="start_date" value="{{request()->get('start_date')}}">
+                                <input type="datetime" class="form-control"  name="start_date" value="{{request()->get('start_date')}}">
                             </div>
                         </div>
                         <div class="col-md-3">
                             <div class="input-group input-group-static mb-4">
                                 <label for="">EndDate</label>
-                                <input type="datetime-local" class="form-control" name="end_date" value="{{request()->get('end_date')}}">
-                            </div>
+                                <input type="datetime" class="form-control" name="end_date" value="{{request()->get('end_date')}}">
+                                </div>
                         </div>
                         <div class="col-md-3">
                             <button class="btn btn-sm btn-primary" id="search" type="submit">Search</button>
@@ -86,14 +86,6 @@
 </div>
 @endsection
 @section('scripts')
-<script src="{{ asset('admin_app/assets/js/plugins/datatables.js') }}"></script>
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.32/dist/sweetalert2.all.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
-<script src="{{ asset('admin_app/assets/js/plugins/datatables.js') }}"></script>
-
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.32/dist/sweetalert2.all.min.js"></script>
-<script src="{{ asset('admin_app/assets/js/plugins/datatables.js') }}"></script>
 <script>
     if (document.getElementById('users-search')) {
         const dataTableSearch = new simpleDatatables.DataTable("#users-search", {
