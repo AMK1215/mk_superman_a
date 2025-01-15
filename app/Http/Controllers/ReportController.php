0606d5b2580c264b39666cee0c1852ee71086c42
sophia <thangneimawi@hivelocity.co.jp>
2025-01-15 22:28:34 +0700

    fixed datetime

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -40,8 +40,8 @@ class ReportController extends Controller
 
     private function buildQuery(Request $request, $adminId)
     {
-        $startDate = $request->start_date ? Carbon::parse($request->start_date)->format('Y-m-d H:i:s'): $this->carbon->startOfMonth()->toDateTimeString();
-        $endDate = $request->end_date ? Carbon::parse($request->end_date)->format('Y-m-d H:i:s'):        $this->carbon->endOfMonth()->toDateTimeString();
+        $startDate = $request->start_date ? Carbon::parse($request->start_date)->subHours(6)->subMinutes(30)->format('Y-m-d H:i:s') : Carbon::today()->startOfDay()->subHours(6)->subMinutes(30)->format('Y-m-d H:i:s');
+        $endDate = $request->end_date ? Carbon::parse($request->end_date)->subHours(6)->subMinutes(30)->format('Y-m-d H:i:s') :  Carbon::today()->subHours(6)->subMinutes(30)->endOfDay()->format('Y-m-d H:i:s');
         
         $resultsSubquery = Result::select(
             'results.user_id',
@@ -104,8 +104,8 @@ class ReportController extends Controller
 
     private function getPlayerDetails($playerId, $request)
     {
-        $startDate = $request->start_date ? Carbon::parse($request->start_date)->format('Y-m-d H:i:s'): $this->carbon->startOfMonth()->toDateTimeString();
-        $endDate = $request->end_date ? Carbon::parse($request->end_date)->format('Y-m-d H:i:s'):        $this->carbon->endOfMonth()->toDateTimeString();
+        $startDate = $request->start_date ? Carbon::parse($request->start_date)->subHours(6)->subMinutes(30)->format('Y-m-d H:i:s') : Carbon::today()->startOfDay()->subHours(6)->subMinutes(30)->format('Y-m-d H:i:s');
+        $endDate = $request->end_date ? Carbon::parse($request->end_date)->subHours(6)->subMinutes(30)->format('Y-m-d H:i:s') :  Carbon::today()->subHours(6)->subMinutes(30)->endOfDay()->format('Y-m-d H:i:s');
         
         $combinedSubquery = DB::table('results')
             ->select(
