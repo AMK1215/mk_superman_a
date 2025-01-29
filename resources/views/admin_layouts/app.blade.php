e01385454aa4fe1513610ad001860f20da546b5c
sophia <thangneimawi@hivelocity.co.jp>
2025-01-30 01:50:30 +0700

    refactor agent folder

--- resources/views/admin_layouts/app.blade.php
+++ resources/views/admin_layouts/app.blade.php
@@ -38,21 +38,13 @@
     <script src="{{ asset('admin_app/assets/js/plugins/quill.min.js') }}"></script>
     <script src="{{ asset('admin_app/assets/js/plugins/datatables.js') }}"></script>
     <script src="{{ asset('admin_app/assets/js/plugins/datatables.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
 
-    <script>
-        var win = navigator.platform.indexOf('Win') > -1;
-        if (win && document.querySelector('#sidenav-scrollbar')) {
-            var options = {
-                damping: '0.5'
-            }
-            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-        }
-    </script>
     <script src="{{ asset('admin_app/assets/js/material-dashboard.min.js?v=3.0.6') }}"></script>
     <script>
         $(document).ready(function() {
             var today = new Date();
-            var startDate = new Date(today.setHours(0, 0, 0, 0)); 
+            var startDate = new Date(today.setHours(0, 0, 0, 0));
             var endDate = new Date(today.setHours(23, 59, 59, 999));
 
             $('input[name="start_date"]').flatpickr({
@@ -66,25 +58,24 @@
         });
     </script>
     <script>
-        @if(Session::has('success') || Session::has('error'))
+        var errorMessage = @json(session('error'));
+        var successMessage = @json(session('success'));
+        @if(session() -> has('success'))
         Swal.fire({
-            title: '{{ Session::has('
-            success ') ? '
-            Success ' : '
-            Error ' }}',
-            text: '{{ Session::has('
-            success ') ? Session::get('
-            success ') : Session::get('
-            error ') }}',
-            icon: '{{ Session::has('
-            success ') ? '
-            success ' : '
-            error ' }}',
-            confirmButtonText: 'Okay'
+            title: successMessage,
+            icon: "success",
+            showConfirmButton: false,
+            showCloseButton: true,
         });
+        @elseif(session() -> has('error'))
+        Swal.fire({
+            icon: 'error',
+            title: errorMessage,
+            showConfirmButton: false,
+            timer: 1500
+        })
         @endif
     </script>
-
     @yield('scripts')
 
 </body>
--- resources/views/admin_layouts/head.blade.php
+++ resources/views/admin_layouts/head.blade.php
@@ -24,6 +24,7 @@ href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
     <!-- CSS Files -->
     <link id="pagestyle" href="{{ asset('admin_app/assets/css/material-dashboard.css?v=3.0.6') }}" rel="stylesheet" />
     <link rel="stylesheet" href="{{asset('admin_app/assets/css/flatpickr.min.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
 
     <!-- Nepcha Analytics (nepcha.com) -->
     <!-- Nepcha is a easy-to-use web analytics. No cookies and fully compliant with GDPR, CCPA and PECR. -->
