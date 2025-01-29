a5a9b6e29d76d725f568d6eec3030b07412171c4
sophia <thangneimawi@hivelocity.co.jp>
2025-01-30 02:12:43 +0700

    refactor banner text folder

--- resources/views/admin/banner_text/index.blade.php
+++ resources/views/admin/banner_text/index.blade.php
@@ -1,19 +1,4 @@
 @extends('admin_layouts.app')
-@section('styles')
-<style>
-  .transparent-btn {
-    background: none;
-    border: none;
-    padding: 0;
-    outline: none;
-    cursor: pointer;
-    box-shadow: none;
-    appearance: none;
-    /* For some browsers */
-  }
-</style>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.32/dist/sweetalert2.min.css">
-@endsection
 @section('content')
 <div class="row mt-4">
   <div class="col-12">
@@ -73,10 +58,6 @@
 </div>
 @endsection
 @section('scripts')
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.32/dist/sweetalert2.all.min.js"></script>
-
-<script src="{{ asset('admin_app/assets/js/plugins/datatables.js') }}"></script>
 <script>
   if (document.getElementById('banners-search')) {
     const dataTableSearch = new simpleDatatables.DataTable("#banners-search", {
@@ -104,12 +85,6 @@
   };
 </script>
 <script>
-  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-  var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl)
-  })
-</script>
-<script>
   $(document).ready(function() {
     $('.transparent-btn').on('click', function(e) {
       e.preventDefault();
@@ -129,17 +104,4 @@
     });
   });
 </script>
-@if(session()->has('success'))
-<script>
-  Swal.fire({
-    icon: 'success',
-    title: '{{ session('
-    success ') }}',
-    showConfirmButton: false,
-    timer: 1500
-  })
-</script>
-@endif
-
-
 @endsection
