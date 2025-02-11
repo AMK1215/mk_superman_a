5a83090aaf56a37bd7c97e10097c46e1a587a7b9
AMK1215 <aungmyokhaing1215@gmail.com>
2025-02-11 09:15:25 +0630

    report backup updated by amk

--- resources/views/admin_layouts/sidebar.blade.php
+++ resources/views/admin_layouts/sidebar.blade.php
@@ -49,6 +49,21 @@
         @endcan
         @can('master_index')
             <li class="nav-item">
+                <a class="nav-link text-white " href="{{ route('admin.master.index') }}">
+                    <span class="sidenav-mini-icon"> <i class="fa-solid fa-user"></i> </span>
+                    <span class="sidenav-normal  ms-2  ps-1"> Master List</span>
+                </a>
+            </li>
+        @endcan
+        @can('agent_index')
+            <li class="nav-item">
+                <a class="nav-link text-white " href="{{ route('admin.agent.index') }}">
+                    <span class="sidenav-mini-icon"> <i class="fa-solid fa-user"></i> </span>
+                    <span class="sidenav-normal  ms-2  ps-1">Agent List</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
                 <a class="nav-link text-white " href="{{ route('admin.backup_results.index') }}">
                     <span class="sidenav-mini-icon"> <i class="fa-solid fa-chart-column"></i> </span>
                     <span class="sidenav-normal  ms-2  ps-1"> ReportBackUp(result) </span>
@@ -67,21 +82,6 @@
                 <a class="nav-link text-white " href="{{ route('admin.reportv2.index') }}">
                     <span class="sidenav-mini-icon"> <i class="fa-solid fa-chart-column"></i> </span>
                     <span class="sidenav-normal  ms-2  ps-1"> V2-Win/lose Report </span>
-                </a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link text-white " href="{{ route('admin.master.index') }}">
-                    <span class="sidenav-mini-icon"> <i class="fa-solid fa-user"></i> </span>
-                    <span class="sidenav-normal  ms-2  ps-1"> Master List</span>
-                </a>
-            </li>
-        @endcan
-        @can('agent_index')
-            <li class="nav-item">
-                <a class="nav-link text-white " href="{{ route('admin.agent.index') }}">
-                    <span class="sidenav-mini-icon"> <i class="fa-solid fa-user"></i> </span>
-                    <span class="sidenav-normal  ms-2  ps-1">Agent List</span>
                 </a>
             </li>
         @endcan
